f8514c8291a09181ec8e8b2b1ac18b972e8c41ba
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-02-15 08:56:58 -0500

    testing objects

---
+++ 2-tests/4-large-lab/test_school.py
@@ -0,0 +1,6 @@
+import pytest 
+
+def test_email_exception():
+    """test that exception is raised for invalid emails"""
+    with pytest.raises(Exception):
+        assert check_email_format("bademail.com")
